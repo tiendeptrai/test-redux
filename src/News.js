43d08df7e85e77d757d99ff7337ff622f8264261
daint6897 <daint6997.com>
2019-07-15 05:14:07 +0700

    add ms ne

---
+++ src/News.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class News extends Component {
+    render() {
+        return (
+            <div>
+                Day la component news ahihi
+            </div>
+        )
+    }
+}
