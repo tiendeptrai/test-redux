5061c23135df5d897a8d1ad2571fa69015ce9505
tiendeptrai <31687664+tiendeptrai@users.noreply.github.com>
2020-10-17 11:21:34 +0700

    Update formInput.js

--- src/reducers/formInput.js
+++ src/reducers/formInput.js
@@ -1,16 +1,23 @@
-// import { createStore } from "redux";
-// var redux = required('redux');
-// const formInitialState = {
-//     dataFrom : ''
-// };
-// const formReducer = (state = formInitialState, action) => {
-//     switch (action.type) {
-//         case "ADD_DATA":
-//             console.log('bien nhan vao'+ action.dataFormInputs);
-//             return state
-//         default:
-//             return state
-//     } 
-// }
-// var formInput = redux.createStore(formReducer);
-// export default formInput;
+import { createStore } from "redux";
+var redux = required('redux');
+const formInitialState = {
+    dataFrom : ''
+};
+const formReducer = (state = formInitialState, action) => {
+    switch (action.type) {
+        case "ADD_DATA":
+            console.log('bien nhan vao'+ action.dataFormInputs);
+            return state
+        default:
+            return state
+    } 
+  switch (action.type) {
+        case "ADD_DATA_AA":
+            console.log('bien nhan vao AAA'+ action.dataFormInputs);
+            return state
+        default:
+            return state
+    } 
+}
+var formInput = redux.createStore(formReducer);
+export default formInput;
